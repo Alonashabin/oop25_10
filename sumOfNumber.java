8997f4c06de4c1f2bc3c544e145319bad5a955f8
Alonashabin <alonashabin@gmail.com>
2025-07-19 12:36:54 +0530

    Add files via upload

---
+++ sumOfNumber.java
@@ -0,0 +1,16 @@
+package oop25_10;
+import java.util.Scanner;
+public class sumOfNumber {
+	public static void main(String[] args) {
+		Scanner num=new Scanner(System.in);
+		int a,b,c;
+		System.out.println("enter the numbers ");
+		a=num.nextInt();
+		b=num.nextInt();
+		c=a+b;
+		System.out.println("the sum of the number is "+c);
+		
+	}
+	
+
+}
